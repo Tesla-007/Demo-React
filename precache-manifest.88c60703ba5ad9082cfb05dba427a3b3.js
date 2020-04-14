3b453043b2bd853c01c8cde1a96e2c928adbd160
Tesla-007 <yadavanuj109@gmail.com>
2020-04-14 18:09:07 +0530

    Updates

--- precache-manifest.88c60703ba5ad9082cfb05dba427a3b3.js
+++ precache-manifest.88c60703ba5ad9082cfb05dba427a3b3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d0ead96747681a33b3328ff5be9d1b3",
+    "revision": "08663a175d9b75fa773efa60149776b9",
     "url": "/Demo-React/index.html"
   },
   {
-    "revision": "58329d989505cd280011",
+    "revision": "175a4d19ee88c4e0ba77",
     "url": "/Demo-React/static/css/main.168a2729.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Demo-React/static/js/2.0bdfa368.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58329d989505cd280011",
-    "url": "/Demo-React/static/js/main.6a595ba0.chunk.js"
+    "revision": "175a4d19ee88c4e0ba77",
+    "url": "/Demo-React/static/js/main.b99b17ec.chunk.js"
   },
   {
     "revision": "f75d51d75c8bd15995f1",
